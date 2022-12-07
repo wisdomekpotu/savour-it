eb1697c6203461dd22eb36867a58c9036bfe5133
wisdom ekpotu <whizfidel@gmail.com>
2022-12-07 12:56:01 +0100

    update

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Budget from './screens/Budget';
+import Hungry from './screens/Hungry';
 
 const Stack = createNativeStackNavigator();
 export default function App() {
@@ -10,14 +11,33 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator
         screenOptions={{
-          title: '',
+          title: 'Savour It',
+          headerStatusBarHeight: 80,
           // headerShadowVisible: false,
           headerStyle: {
             backgroundColor: '#DA0091',
           },
+          headerTitleAlign: 'center',
+
+          headerTitleStyle: {
+            color: 'white',
+            position: 'absolute',
+            width: 91,
+            height: 18,
+            left: 151,
+            top: 68,
+            // fontFamily: 'Lactosa',
+            fontStyle: 'normal',
+            fontWeight: '400',
+            fontSize: 18.3182,
+            lineHeight: 18,
+            border: '2.36364px solid #DA0091',
+            textShadow: '0px 2.36364px 0px rgba(218, 0, 145, 0.25)',
+          },
         }}
       >
         <Stack.Screen name='Budget' component={Budget} />
+        <Stack.Screen name='Hungry' component={Hungry} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- screens/Budget.js
+++ screens/Budget.js
@@ -1,12 +1,65 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 
-export default function Budget() {
+export default function Budget({ navigation }) {
   return (
     <View>
-      <Text>Budget</Text>
+      <Text style={styles.text}>Do you have a {'\n\n'} budget?</Text>
+      <Pressable
+        style={styles.button}
+        onPress={() => navigation.navigate('Hungry')}
+      >
+        <Text style={styles.button_text}>$10 - $20</Text>
+      </Pressable>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  text: {
+    color: '#4D0033',
+    position: 'absolute',
+    width: 273,
+    height: 64,
+    left: 60,
+    top: 68,
+    fontfamily: 'Clash Display',
+    fontStyle: 'normal',
+    fontWeight: '600',
+    fontSize: 35,
+    lineHeight: 30,
+    textAlign: 'center',
+  },
+
+  button: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    paddingTop: 20,
+    paddingBottom: 20,
+    paddingLeft: 50,
+    paddingRight: 50,
+    gap: 10,
+    position: 'absolute',
+    width: 182,
+    height: 64,
+    left: 105,
+    top: 307,
+    backgroundColor: '#DA0091',
+    borderRadius: 80,
+  },
+
+  button_text: {
+    width: 82,
+    height: 24,
+    fontStyle: 'normal',
+    fontWeight: '800',
+    fontSize: 20,
+    textAlign: 'center',
+    lineHeight: 24,
+    color: '#FFFFFF',
+    // flex: 'none',
+    order: 0,
+    flexGrow: 0,
+  },
+});
